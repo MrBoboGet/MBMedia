22f6c914afb2b710f5a73a2b0d6648b31b16bbdc
MrBoboGet <emanuelberggren01@gmail.com>
2021-12-17 05:52:41 +0100

    Snart klar med nya interfacen. Kan vara så att jag gjort fel med timebas konverteringen i gammla varianten

--- MBMedia2.cpp
+++ MBMedia2.cpp
@@ -1,8 +1,273 @@
 #include "MBMedia2.h"
-
+#ifdef _WIN32
+#include <unknwn.h>
+#include<strmif.h>
+#endif // __WIN32__
+#include <vector>
+#include "MBMedia.h"
+#include <iostream>
+#include <filesystem>
+#include <assert.h>
+extern "C"
+{
+#include <ffmpeg/libavcodec/avcodec.h>
+#include <ffmpeg/libavformat/avformat.h>
+//#include <ffmpeg/libavutil/>
+}
 namespace MBMedia
 {
+	void h_Print_ffmpeg_Error(int ReadFrameResponse)
+	{
+		if (ReadFrameResponse >= 0)
+		{
+			return;
+		}
+		char MessageBuffer[512];
+		av_strerror(ReadFrameResponse, MessageBuffer, 512);
+		std::cout << "FFMpeg error: " << MessageBuffer << std::endl;
+	}
+	int FFMPEGCall(int ReadFrameResponse)
+	{
+		h_Print_ffmpeg_Error(ReadFrameResponse);
+		return(ReadFrameResponse);
+	}
+	void _FreeFormatContext(void* DataToFree)
+	{
+		avformat_free_context((AVFormatContext*)DataToFree);
+		//FFMPEGCall();
+	}
+	MediaType h_FFMPEGMediaTypeToMBMediaType(AVMediaType TypeToConvert)
+	{
+		MediaType ReturnValue = MediaType::Null;
+		if (TypeToConvert == AVMEDIA_TYPE_VIDEO)
+		{
+			ReturnValue = MediaType::Video;
+		}
+		else if (TypeToConvert == AVMEDIA_TYPE_AUDIO)
+		{
+			ReturnValue = MediaType::Audio;
+		}
+		else if (TypeToConvert == AVMEDIA_TYPE_SUBTITLE)
+		{
+			ReturnValue = MediaType::Subtitles;
+		}
+		return(ReturnValue);
+	}
 
+	//BEGIN StreamInfo
+	StreamInfo::StreamInfo(std::shared_ptr<void> FFMPEGContainerData, size_t StreamIndex)
+	{
+		m_InternalData = FFMPEGContainerData;
+		m_StreamIndex = -StreamIndex;
+	}
+
+	//END StreamInfo
+
+	void _FreePacket(void* PacketToFree)
+	{
+		AVPacket* Packet = (AVPacket*)PacketToFree;
+		av_packet_free(&Packet);
+	}
+	//BEGIN StreamPacket
+	StreamPacket::StreamPacket(void* FFMPEGPacket,MediaType PacketType)
+		//: m_ImplementationData(FFMPEGPacket, _FreePacket)
+	{
+		if (FFMPEGPacket != nullptr)
+		{
+			m_Type = PacketType;
+			m_InternalData = std::unique_ptr<void, void(*)(void*)>(FFMPEGPacket, _FreePacket);
+		}
+	}
+	//END StreamPacket
+
+
+	//BEGIN ContainerDemuxer
+	ContainerDemuxer::ContainerDemuxer(std::string const& InputFile)
+	{
+		AVFormatContext* InputFormatContext;
+		InputFormatContext = avformat_alloc_context();
+		//avformat_new_stream
+		//allokerar format kontexten, information om filtyp och innehåll,läser bara headers och etc
+		FFMPEGCall(avformat_open_input(&InputFormatContext, InputFile.c_str(), NULL, NULL));
+		//läsar in data om själva datastreamsen
+		FFMPEGCall(avformat_find_stream_info(InputFormatContext, NULL));
+		m_InternalData = std::shared_ptr<void>(InputFormatContext, _FreeFormatContext);
+		for (size_t i = 0; i < InputFormatContext->nb_streams; i++)
+		{
+			m_InputStreams.push_back(StreamInfo(m_InternalData, i));//hacky af, sparar hela decode contexten eftersom free_stream inte är en del av en public header
+		}
+	}
+	//bool ContainerDemuxer::EndOfFile()
+	//{
+	//	return(true);
+	//}
+	StreamInfo const& ContainerDemuxer::GetStreamInfo(size_t StreamIndex)
+	{
+		return(m_InputStreams[StreamIndex]);
+	}
+	StreamPacket ContainerDemuxer::GetNextPacket(size_t* StreamIndex)
+	{
+		AVPacket* NewPacket = av_packet_alloc();
+		int ReadResponse = FFMPEGCall(av_read_frame((AVFormatContext*)m_InternalData.get(),NewPacket));
+		AVFormatContext* InputContext = (AVFormatContext*)m_InternalData.get();
+		if (ReadResponse >= 0)
+		{
+			return(StreamPacket(NewPacket, h_FFMPEGMediaTypeToMBMediaType(InputContext->streams[NewPacket->stream_index]->codecpar->codec_type)));
+		}
+		else 
+		{
+			return(StreamPacket(nullptr, MediaType::Null));
+		}
+	}
+	//END ContainerDemuxer
+
+	//BEGIN OutputContext
+	OutputContext::OutputContext(std::string const& OutputFile)
+	{
+
+	}
+
+	void OutputContext::AddOutputStream(StreamEncoder&& Encoder)
+	{
+		//m_OutputEncoders.push_back(Encoder);
+	}
+	void OutputContext::WriteHeader()
+	{
+		AVFormatContext* OutputFormatContext = (AVFormatContext*)m_InternalData.get();
+		if (OutputFormatContext == nullptr)
+		{
+			return;
+		}
+		avformat_write_header(OutputFormatContext, NULL);
+	}
+	void OutputContext::InsertFrame(StreamFrame const& FrameToInsert, size_t StreamIndex)
+	{
+		m_OutputEncoders[StreamIndex].InsertFrame(FrameToInsert);
+		while (true)
+		{
+			StreamPacket NewPacket = m_OutputEncoders[StreamIndex].GetNextPacket();
+			if (NewPacket.GetType() == MediaType::Null)
+			{
+				break;
+			}
+			//int hej = 0 / 0;
+			p_WritePacket(NewPacket, StreamIndex);
+		}
+	}
+	void OutputContext::Finalize()
+	{
+		for (size_t i = 0; i < m_OutputEncoders.size(); i++)
+		{
+			m_OutputEncoders[i].Flush();
+			while (true)
+			{
+				StreamPacket NewPacket = m_OutputEncoders[i].GetNextPacket();
+				if (NewPacket.GetType() == MediaType::Null)
+				{
+					break;
+				}
+				p_WritePacket(NewPacket, i);
+			}
+		}
+		p_WriteTrailer();
+	}
+	//END OutputContext
+	void _FreeFrame(void* FFMPEGFrameToFree) 
+	{
+		AVFrame* Frame =(AVFrame*)FFMPEGFrameToFree;
+		av_frame_free(&Frame);
+	}
+	//StreamFrame
+	StreamFrame::StreamFrame(void* FFMPEGData,MediaType FrameType)
+	{
+		if (FFMPEGData != nullptr)
+		{
+			m_InternalData = std::unique_ptr<void, void(*)(void*)>(FFMPEGData, _FreeFrame);
+			m_MediaType = FrameType;
+		}
+	}
+	//StreamFrame
+
+
+	//BEGIN StreamDecoder
+	void _FreeCodecContext(void* PointerToFree)
+	{
+		AVCodecContext* CodecContext = (AVCodecContext*)PointerToFree;
+		avcodec_free_context(&CodecContext);
+	}
+	StreamDecoder::StreamDecoder(StreamInfo const& StreamToDecode)
+	{
+		AVFormatContext* ContainerFormat = (AVFormatContext*)StreamToDecode.m_InternalData.get();
+		AVCodecParameters* NewInputCodecParamters = ContainerFormat->streams[StreamToDecode.m_StreamIndex]->codecpar;
+		AVCodec* NewInputCodec = avcodec_find_decoder(NewInputCodecParamters->codec_id);
+		AVCodecContext* NewCodexContext = avcodec_alloc_context3(NewInputCodec);
+		FFMPEGCall(avcodec_parameters_to_context(NewCodexContext, NewInputCodecParamters));
+		//sedan måste vi öppna den, vet inte riktigt varför, initializerar den kanske?
+		FFMPEGCall(avcodec_open2(NewCodexContext, NewInputCodec, NULL));
+		m_InternalData = std::shared_ptr<void>(NewCodexContext, _FreeCodecContext);
+		m_Type = h_FFMPEGMediaTypeToMBMediaType(NewInputCodec->type);
+		//DecodeCodecContext.push_back(NewCodexContext);
+	}
+	void StreamDecoder::InsertPacket(StreamPacket const& PacketToDecode)
+	{
+		AVCodecContext* CodecContext = (AVCodecContext*)m_InternalData.get();
+		FFMPEGCall(avcodec_send_packet(CodecContext, (const AVPacket*)PacketToDecode.m_InternalData.get()));
+	}
+	StreamFrame StreamDecoder::GetNextFrame()
+	{
+		AVCodecContext* CodecContext = (AVCodecContext*)m_InternalData.get();
+		AVFrame* NewFrame = av_frame_alloc();
+		int RecieveResult = FFMPEGCall(avcodec_receive_frame(CodecContext, NewFrame));
+		MediaType FrameType = m_Type;
+		if (RecieveResult < 0)
+		{
+			av_frame_free(&NewFrame);
+			FrameType = MediaType::Null;
+		}
+		return(StreamFrame(NewFrame,m_Type));
+	}
+	void StreamDecoder::Flush()
+	{
+		AVCodecContext* CodecContext = (AVCodecContext*)m_InternalData.get();
+		FFMPEGCall(avcodec_send_packet(CodecContext, nullptr));
+	}
+
+	//END StreamDecoder
+
+	//BEGIN StreamEncoder
+
+	StreamEncoder::StreamEncoder(Codec StreamType, AudioEncodeInfo const& EncodeInfo)
+	{
+
+	}
+	StreamEncoder::StreamEncoder(Codec StreamType, VideoEncodeInfo const& EncodeInfo)
+	{
+
+	}
+	void StreamEncoder::InsertFrame(StreamFrame const& FrameToEncode)
+	{
+		AVCodecContext* CodecContext = (AVCodecContext*)m_InternalData.get();
+		FFMPEGCall(avcodec_send_frame(CodecContext,(const AVFrame*) FrameToEncode.m_InternalData.get()));
+	}
+	void StreamEncoder::Flush()
+	{
+		AVCodecContext* CodecContext = (AVCodecContext*)m_InternalData.get();
+		FFMPEGCall(avcodec_send_frame(CodecContext, nullptr));
+	}
+	StreamPacket StreamEncoder::GetNextPacket()
+	{
+		AVCodecContext* CodecContext = (AVCodecContext*)m_InternalData.get();
+		AVPacket* NewPacket = av_packet_alloc();
+		MediaType PacketType = m_Type;
+		int FFmpegResult = FFMPEGCall(avcodec_receive_packet(CodecContext,NewPacket));
+		if (FFmpegResult < 0)
+		{
+			av_packet_free(&NewPacket);
+		}
+		return(StreamPacket(NewPacket, PacketType));
+	}
+
+	//END StreamEncoder
 
 	void Transcode(std::string const& InputFile, std::string const& OutputFile, Codec NewAudioCodec, Codec NewVideoCodec)
 	{
@@ -11,34 +276,46 @@ namespace MBMedia
 		OutputContext OutputData(OutputFile);
 		for (size_t i = 0; i < InputData.NumberOfStreams(); i++)
 		{
-			if (InputData.GetStreamInfo(i).Type == MediaType::Audio)
+			if (InputData.GetStreamInfo(i).GetMediaType() == MediaType::Audio)
 			{
 				Decoders.push_back(StreamDecoder(InputData.GetStreamInfo(i)));
 				OutputData.AddOutputStream(StreamEncoder(NewAudioCodec,GetAudioEncodePresets(Decoders.back())));
 			}
-			if (InputData.GetStreamInfo(i).Type == MediaType::Video)
+			if (InputData.GetStreamInfo(i).GetMediaType() == MediaType::Video)
 			{
 				Decoders.push_back(StreamDecoder(InputData.GetStreamInfo(i)));
 				OutputData.AddOutputStream(StreamEncoder(NewVideoCodec, GetVideoEncodePresets(Decoders.back())));
 			}
 		}
-		while (!InputData.EndOfFile())
+		while (true)
 		{
 			size_t PacketIndex = 0;
 			StreamPacket NewPacket = InputData.GetNextPacket(&PacketIndex);
+			if (NewPacket.GetType() == MediaType::Null)
+			{
+				break;
+			}
 			Decoders[PacketIndex].InsertPacket(NewPacket);
-			while (Decoders[PacketIndex].DataAvailable())
+			while (true)
 			{
 				StreamFrame NewFrame = Decoders[PacketIndex].GetNextFrame();
+				if (NewFrame.GetMediaType() == MediaType::Null)
+				{
+					break;
+				}
 				OutputData.InsertFrame(NewFrame, PacketIndex);
 			}
 		}
 		for (size_t i = 0; i < Decoders.size(); i++)
 		{
 			Decoders[i].Flush();
-			while (Decoders[i].DataAvailable())
+			while (true)
 			{
 				StreamFrame NewFrame = Decoders[i].GetNextFrame();
+				if (NewFrame.GetMediaType() == MediaType::Null)
+				{
+					break;
+				}
 				OutputData.InsertFrame(NewFrame, i);
 			}
 		}
--- MBMedia2.h
+++ MBMedia2.h
@@ -1,6 +1,10 @@
-#include <memory>
+#pragma once
 #include <vector>
+#include <memory>
 #include <string>
+#include <stddef.h>
+#include <cstdint>
+#include <stdint.h>
 namespace MBMedia
 {
 	enum class Codec
@@ -25,14 +29,21 @@ namespace MBMedia
 		Null,
 	};
 
-
+	//har inte hittat entry pointen till free_steam, så gör något lite hacky med att låta streamen vara helt kopplad till format kontexten
+	void _FreeFormatContext(void* DataToFree);
 	struct StreamInfo
 	{
 	private:
-
+		friend class ContainerDemuxer;
+		friend class StreamDecoder;
+		size_t m_StreamIndex = -1;//all detta förutsätter att stream datan  inte förändras
+		std::shared_ptr<void> m_InternalData = nullptr;
+		StreamInfo(std::shared_ptr<void> FFMPEGContainerData,size_t StreamIndex); //används av decoder, en decode context har en konstant mängd streams
+		MediaType m_Type = MediaType::Null;
+		Codec m_StreamCodec = Codec::Null;
 	public:
-		MediaType Type = MediaType::Null;
-		Codec StreamCodec = Codec::Null;
+		MediaType GetMediaType() const {return(m_Type);};
+		Codec GetCodec() const {return(m_StreamCodec);};
 	};
 
 	struct VideoDecodeInfo
@@ -51,73 +62,111 @@ namespace MBMedia
 	{
 
 	};
-
+	inline void _DoNothing(void*)
+	{
+		return;
+	}
+	void _FreePacket(void*);
 	class StreamPacket
 	{
 	private:
-		void* m_ImplementationData = nullptr;
+		friend class ContainerDemuxer;
+		friend class StreamDecoder;
+		friend class StreamEncoder;
+		std::unique_ptr<void, void (*)(void*)> m_InternalData = std::unique_ptr<void, void (*)(void*)>(nullptr, _DoNothing);
 		MediaType m_Type = MediaType::Null;
+		StreamPacket(void* FFMPEGPacket,MediaType PacketType);
 	public:
+		StreamPacket(StreamPacket const&) = delete;
+		StreamPacket(StreamPacket&&) = default;
+		StreamPacket& operator=(StreamPacket&&) = default;
 		MediaType GetType() { return(m_Type); }
-		~StreamPacket();
+		//~StreamPacket();
 	};
 	class StreamFrame
 	{
 	private:
+		friend class StreamDecoder;
+		friend class StreamEncoder;
+		std::unique_ptr<void, void (*)(void*)> m_InternalData = std::unique_ptr<void, void (*)(void*)>(nullptr, _DoNothing);
 		MediaType m_MediaType = MediaType::Null;
+		StreamFrame(void* FFMPEGData,MediaType FrameType);
 	public:
 		MediaType GetMediaType() { return(m_MediaType); };
 	};
+
+	void _FreeCodecContext(void*);
 	class StreamDecoder
 	{
 	private:
 		//void p_Flush();
+		std::shared_ptr<void> m_InternalData = nullptr;
+		MediaType m_Type = MediaType::Null;
 	public:
+		StreamDecoder(StreamDecoder const&) = delete;
+		StreamDecoder(StreamDecoder&&) = default;
+		StreamDecoder& operator=(StreamDecoder&&) = default;
+		
 		StreamDecoder(StreamInfo const& StreamToDecode);//implicit antagande här, att decoda en stream kan göras "korrekt", att omvandla eller omtolka datan bör göras från framesen vi får efter
 
 		void InsertPacket(StreamPacket const& PacketToDecode);
-		bool DataAvailable();
 		StreamFrame GetNextFrame();
 		void Flush();
-		~StreamDecoder();
+		//~StreamDecoder();
 	};
 	class StreamEncoder
 	{
 	private:
 
+		std::unique_ptr<void, void (*)(void*)> m_InternalData = std::unique_ptr<void, void (*)(void*)>(nullptr,_DoNothing);
+		MediaType m_Type = MediaType::Null;
 	public:
+		StreamEncoder(StreamEncoder const&) = delete;
+		StreamEncoder(StreamEncoder&&) = default;
+		StreamEncoder& operator=(StreamEncoder&&) = default;
+
 		StreamEncoder(Codec StreamType, AudioEncodeInfo const& EncodeInfo);
 		StreamEncoder(Codec StreamType, VideoEncodeInfo const& EncodeInfo);
+
 		void InsertFrame(StreamFrame const& FrameToEncode);
 		void Flush();
 		StreamPacket GetNextPacket();
-		~StreamEncoder();
+		
+		MediaType GetMediaType() { return(m_Type); };
+		//~StreamEncoder();
 	};
 	class ContainerDemuxer
 	{
 	private:
+		std::shared_ptr<void> m_InternalData = nullptr;
 		std::vector<StreamInfo> m_InputStreams = {};
 		bool m_FileEnded = false;
 	public:
 		size_t NumberOfStreams() { return(m_InputStreams.size()); }
 		StreamInfo const& GetStreamInfo(size_t StreamIndex);
 		StreamPacket GetNextPacket(size_t* StreamIndex);
-		bool EndOfFile();
+		//bool EndOfFile();
 
 		ContainerDemuxer(std::string const& InputFile);
-		~ContainerDemuxer();
+		//~ContainerDemuxer();
 	};
 	class OutputContext
 	{
 	private:
 		//std::vector<std::unique_ptr<
 		ContainerFormat m_OutputFormat = ContainerFormat::Null;
-		std::vector<StreamInfo> m_OutputStreams = {};
+		std::unique_ptr<void, void (*)(void*)> m_InternalData = std::unique_ptr<void, void (*)(void*)>(nullptr, _DoNothing);
+		std::vector<StreamEncoder> m_OutputEncoders = {};
+		void p_WritePacket(StreamPacket const& PacketToWrite, size_t StreamIndex);
+		void p_WriteTrailer();
 	public:
 		OutputContext(std::string const& OutputFile);
 
+
 		void AddOutputStream(StreamEncoder&& Encoder);
 		void WriteHeader();
+		
+		//ffmpeg verkar inte ha någon stream data i framen, kanske innebär att framen på något sätt är atomisk...
 		void InsertFrame(StreamFrame const& PacketToInsert, size_t StreamIndex);
 		void Finalize();
 		~OutputContext();
